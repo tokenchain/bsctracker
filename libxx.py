5b39ec2d9ea59816a44355128432ed639f83a03a
heskdx <jobhesk@gmail.com>
2023-06-04 02:36:23 +0800

    ♻️  3 files changed, 69 insertions(+), 70 deletions(-) work process treemap

--- libxx.py
+++ libxx.py
@@ -1,4 +1,7 @@
 import requests
+import graphviz
+import math
+
 
 api_key = "59IISNUG2CR3PIMAJF7MACQTS7DY6WFJDJ"
 url = 'https://api.bscscan.com/api'
@@ -110,4 +113,65 @@ def do_list_contract(_account: str, page_d: int = 0):
     except Exception as e:
         print(f"got error from the explorer error exception -> {_account}, {e}")
         return _account
+
+
+
+def find_key(dictsx:list, address:str)->int:
+    for i, item in enumerate(dictsx):
+        if item["up"]==address:
+            return i
+    return -1
+
+def bsc_relation_read(dot):
+    a = Filex()
+    addresses = a.openRelation()
+    d_count = 0
+    container = []
+    for from_address in addresses:
+        child = from_address[0]
+        parent = from_address[1]
+
+        y = find_key(container, parent)
+        if y > -1:
+            container[y]["children"]+=1
+            container[y]["down"].append(child)
+        else:
+            # container[parent]["children"] = 0
+            container.append({
+                "up":parent,
+                "children":0,
+                "down":[],
+            })
+            
+    container = sorted(container, key=lambda x: -x["children"])
+  
+    for c in container:
+        up = c["up"]
+        for dow in c["down"]:
+            dot.edge(up, dow)
+    #for level in container:
+    #dot.edge(parent, child)
+    #print(f"The total edges is 0")
+    return dot
+
+def graph_building_bsc_relations(project_name:str):
+    dot = graphviz.Digraph(
+        project_name,
+        comment='Sun',
+        engine='dot',
+        format='pdf'
+    )
+    dot.attr(
+        ordering='out',
+        k='2.2',
+        overlap='prism0',
+        rankdir='LR',
+        size='1000,250'
+    )
+    # dot.attr('node', shape='doublecircle')
+    # dot.format = 'pdf'
+    # dot.engine = 'dot'
+    # dot.engine = 'neato'
+    dot = bsc_relation_read(dot)
+    dot.render(directory='data/charts').replace('\\', '/')
 
